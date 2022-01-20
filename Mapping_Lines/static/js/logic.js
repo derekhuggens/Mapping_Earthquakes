41a48a7c305b92863498fe9fd89e8eacf3943ce5
derekhuggens <derekhuggens@gmail.com>
2022-01-20 14:29:25 -0500

    tracking Mapping_Lines

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,32 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+
+// Coordinates for each point to be used in the polyline
+
+let line = [
+    [37.6211, -122.3711],
+    [30.1911, -97.6611],
+    [43.6711, -79.6211],
+    [40.6411, -73.7711]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    weight: 4,
+    opacity: 0.5,
+    dashArray : '15,10',
+}).addTo(map);
+
+  // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
